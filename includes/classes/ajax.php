4f53b595352e47940eabffe6dcae656c29a27ac1
Armanul46 <arman.sovware@gmail.com>
2021-10-04 16:10:37 +0600

    work on sortable

--- includes/classes/ajax.php
+++ includes/classes/ajax.php
@@ -9,8 +9,8 @@ class PGCU_Ajax
 
 		add_action('wp_ajax_pgcu_post_type', array( $this, 'post_type_ajax_handler') ); // wp_ajax_{action}
 
-        add_action('wp_ajax_lsu_loadmore', array( $this, 'sortable_ajax_handler') ); // wp_ajax_{action}
-        add_action('wp_ajax_nopriv_lsu_loadmore', array( $this, 'sortable_ajax_handler') );
+        add_action('wp_ajax_pgcu_sortable', array( $this, 'sortable_ajax_handler') ); // wp_ajax_{action}
+        add_action('wp_ajax_nopriv_pgcu_sortable', array( $this, 'sortable_ajax_handler') );
 
     }
 
@@ -33,17 +33,17 @@ class PGCU_Ajax
         die; // here we exit the script and even no wp_reset_query() required!
     }
 
-    function sortable_ajax_handler(){
-        ob_start();
-
-        $term_id = ! empty( $_POST['term_id'] ) ? $_POST['term_id'] : 'all';
+    function sortable_ajax_handler() {
+        $term_id      = ! empty( $_POST['term_id'] ) ? $_POST['term_id'] : 'all';
         $post_id      = ! empty( $_POST['id']  ) ? $_POST['id'] : '';
-        $query   = ! empty( $_POST['query']  ) ? json_decode( stripslashes( $_POST['query'] ), true ) : '';
+        $query        = ! empty( $_POST['query']  ) ? json_decode( stripslashes( $_POST['query'] ), true ) : '';
+        
         $data 	 = get_post_meta( $post_id, 'gc', true );
         $data = post_grid_and_carousel_ultimate::unserialize_and_decode24( $data );
         $value = is_array( $data ) ? $data : array();
         extract( $value );
 
+        $theme                = ! empty( $theme ) ? $theme : 'theme-1';
         $post_type            = ! empty( $post_type ) ? $post_type : 'post';
         $term_from            = ! empty( $term_from ) ? $term_from : 'category';
         $display_term         = ! empty( $display_term ) ? $display_term : 'yes';
@@ -60,10 +60,43 @@ class PGCU_Ajax
         $image_width	   = ! empty( $image_width ) ? $image_width : 300;
         $image_height	   = ! empty( $image_height ) ? $image_height : 290;
 
-        
-        $value = ob_get_clean();
+        $g_sort               = ! empty( $g_sort    ) ? $g_sort    : 'category';
 
-        wp_send_json( $value ); 
+        $query['tax_query'] = array( 
+            array(
+            'taxonomy' => $g_sort,
+            'field' => 'term_id',
+            'terms' => $term_id,
+            )
+        );
+
+        // it is always better to use WP_Query but not here
+        query_posts( $query );
+
+        
+        if( have_posts() ) :
+     
+            // run the loop
+            while( have_posts() ): the_post();
+
+            $thumb = get_post_thumbnail_id();
+            // crop the image if the cropping is enabled.
+            if( 'yes' === $image_resize_crop ){
+                $pgcu_img = pgcu_image_cropping( $thumb, $image_width, $image_height, true, 100 )['url'];
+            }else{
+                $aazz_thumb = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID()), 'large' );
+                $pgcu_img = $aazz_thumb['0'];
+            }
+
+            $get_terms  = get_the_terms( get_the_ID(), $term_from );
+            $post_views = get_post_meta( get_the_id(), '_pgcu_post_views_count', true );
+     
+            include PGCU_INC_DIR . 'templates/' . $theme . '.php';
+     
+     
+            endwhile;
+     
+        endif;
 
         die; // here we exit the script and even no wp_reset_query() required!
     }

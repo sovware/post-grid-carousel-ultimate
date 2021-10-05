0918b6b6a1918efc56b07495422d83f6b9df62a0
Armanul46 <arman.sovware@gmail.com>
2021-10-05 12:29:04 +0600

    added sortable variable

--- includes/templates/sortable/sortable.php
+++ includes/templates/sortable/sortable.php
@@ -1,14 +1,8 @@
 <?php if( $terms ) { ?>
 <div class="pgcu-post-sortable__nav" style="
-    --pgcu-pagColor: <?php echo $pagi_color; ?>;
-    --pgcu-pagColorHover: <?php echo $pagi_hover_color; ?>;
-    --pgcu-pagBorderColor: <?php echo $pagi_border_color; ?>;
-    --pgcu-pagBorderColorHover: <?php echo $pagi_hover_border_color; ?>;
-    --pgcu-pagBgColor: <?php echo $pagi_back_color; ?>;
-    --pgcu-pagBgColorHover: <?php echo $pagi_hover_back_color; ?>;
-    --pgcu-pagActiveColor: <?php echo $pagi_active_color; ?>;
-    --pgcu-pagActiveBorderColor: <?php echo $pagi_active_border_color; ?>;
-    --pgcu-pagActiveBgColor: <?php echo $pagi_active_back_color; ?>;
+    --pgcu-sortMenuTextColor: <?php echo $sortable_menu_text_color; ?>;
+    --pgcu-sortMenuActiveBackColor: <?php echo $sortable_menu_active_back_color; ?>;
+    --pgcu-sortMenuActiveTextColor: <?php echo $sortable_menu_active__text_color; ?>;
 ">
     <a href="" class="pgcu-post-sortable__btn pgcu-post-sortable__btn--active" data-sortable-nav="all">All (<?php echo $posts->found_posts; ?>)</a>
 

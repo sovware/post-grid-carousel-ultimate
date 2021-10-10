49eedb8503971430489545869af7a21fc2cbff8a
Armanul46 <arman.sovware@gmail.com>
2021-10-10 19:04:49 +0600

    testing

--- includes/support.php
+++ includes/support.php
@@ -11,17 +11,17 @@
             <br>
 
             <h3>Support Forum</h3>
-            <p> If you need any helps, please don't hesitate to post it on <a href="https://wordpress.org/support/plugin/post-grid-carousel-ultimate" target="_blank"> wordpress.org</a> or <a href="https://aazztech.com/support" target="_blank"> aazztech.com</a> support forum.</p>
+            <p> If you need any helps, please don't hesitate to post it on <a href="https://wordpress.org/support/plugin/post-grid-carousel-ultimate" target="_blank"> wordpress.org</a> or <a href="https://wpwax.com/support" target="_blank"> wpwax.com</a> support forum.</p>
             <br>
 
             <h3>More features</h3>
-            <p><p>Upgrading to the <a href="https://aazztech.com/product/post-grid-carousel-ultimate-pro/" target="_blank">Premium Version</a> would unlock more amazing features of the plugin.</p></p>
+            <p><p>Upgrading to the <a href="https://wpwax.com/product/post-grid-carousel-ultimate-pro/" target="_blank">Premium Version</a> would unlock more amazing features of the plugin.</p></p>
             <p>
-                <a class="button button-primary" href="https://aazztech.com/demos/plugins/post-grid-carousel-ultimate-pro-screenshots/" target="_blank">Backend Screenshots</a>
+                <a class="button button-primary" href="https://wpwax.com/demos/plugins/post-grid-carousel-ultimate-pro-screenshots/" target="_blank">Backend Screenshots</a>
 
-                <a class="button button-primary" href="https://aazztech.com/demos/plugins/post-carousel-theme-4/" target="_blank">Live Preview</a>
+                <a class="button button-primary" href="https://wpwax.com/demos/plugins/post-carousel-theme-4/" target="_blank">Live Preview</a>
 
-                <a class="button button-primary" href="https://aazztech.com/product/post-grid-carousel-ultimate-pro/" target="_blank">Upgrade to Pro</a>
+                <a class="button button-primary" href="https://wpwax.com/product/post-grid-carousel-ultimate-pro/" target="_blank">Upgrade to Pro</a>
             </p>
 
         </div>
@@ -33,24 +33,24 @@
 
             <h3>Need more features?</h3>
             <ul class="adl_pro_features">
-                <li>24 different themes for Grid, Filterable Grid and Masonry!</li>
-                <li>4 different themes for carousel.</li>
-                <li>4 different themes for widget.</li>
+                <li>80 different themes for Grid, Filterable Grid and Masonry!</li>
+                <li>20 different themes for carousel.</li>
+                <li>20 different themes for grid.</li>
+                <li>20 different themes for sortable grid.</li>
+                <li>20 different themes for masonry.</li>
                 <li>Display popular, featured, latest, older and random posts.</li>
                 <li>Display posts by category, IDs, tag, month and year.</li>
                 <li>Control font size, weight, alignment of post title and content.</li>
-                <li>Lots of options to customize posts in widget.</li>
                 <li>Excerpt length control.</li>
                 <li>Ajax Load More pagination.</li>
                 <li>Social share buttons.</li>
                 <li>Show/hide navigation.</li>
                 <li>Navigational arrows color changing option.</li>
-                <li>Show/Hide Carousel dots pagination.</li>
                 <li>Read more button edit option.</li>
                 <li>And many more features.</li>
             </ul>
-            <p class="text-center"><a target="_blank" href="https://aazztech.com/product/post-grid-carousel-ultimate-pro/" class="button button-primary">Click here to know more</a></p>
-            <p class="text-center"><a target="_blank" href="https://aazztech.com/product/post-grid-carousel-ultimate-pro/" class="button button-primary">Get the Pro Version Now!</a></p>
+            <p class="text-center"><a target="_blank" href="https://wpwax.com/product/post-grid-carousel-ultimate-pro/" class="button button-primary">Click here to know more</a></p>
+            <p class="text-center"><a target="_blank" href="https://wpwax.com/product/post-grid-carousel-ultimate-pro/" class="button button-primary">Get the Pro Version Now!</a></p>
         </div>
     </div>
 </div>

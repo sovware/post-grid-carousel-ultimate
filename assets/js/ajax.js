13feba4309623f07cd5937b299034e92ef1ae8ec
Shamim Ahmed <shamimeforb@gmail.com>
2021-10-11 13:19:38 +0600

    Fixed: style issue

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -1,24 +1,30 @@
-(function($) {
+(function ($) {
 
-    "use strict";
+	"use strict";
 
-    $('.pgcu-post-sortable__btn').on("click", function(e){
-		e.preventDefault();
-		var button = $(this),
-		    data = {
-			'action'	: 'pgcu_sortable',
-			'id'		: $(this).attr('data-id'),
-            'term_id'	: $(this).attr('data-sortable-nav'),
-			'query'	    : pgcu_ajax.query
-		};
-		$.ajax({ // you can also use $.post here
-			url : pgcu_ajax.ajaxurl, // AJAX handler
-			data : data,
-			type : 'POST',
-			success : function( data ){
-				$('.pgcu-row').empty().append( data );
-			}
+	$('.pgcu-post-sortable__btn').each(function(id, elm){
+		$(elm).on("click", function (e) {
+			e.preventDefault();
+			var button = $(this),
+				data = {
+					'action': 'pgcu_sortable',
+					'id': $(this).attr('data-id'),
+					'term_id': $(this).attr('data-sortable-nav'),
+					'query': pgcu_ajax.query
+				};
+			$(elm).closest('.pgcu-post-sortable__nav').siblings('.pgcu-row').addClass('pgcu-post-loading');
+			$.ajax({ // you can also use $.post here
+				url: pgcu_ajax.ajaxurl, // AJAX handler
+				data: data,
+				type: 'POST',
+				success: function (data) {
+					$(elm).closest('.pgcu-post-sortable__nav').siblings('.pgcu-row').empty().append(data);
+					$(elm).siblings().removeClass('pgcu-post-sortable__btn--active');
+					$(elm).addClass('pgcu-post-sortable__btn--active');
+					$(elm).closest('.pgcu-post-sortable__nav').siblings('.pgcu-row').removeClass('pgcu-post-loading');
+				}
+			});
 		});
-	});
+	})
 
 })(jQuery);

109a93efe4598e73760cd922dca0489c6155674e
Shamim Ahmed <shamimeforb@gmail.com>
2021-10-11 11:02:22 +0600

    Fixed: masonry load more issue; Removed: ajax.js file and moved it's code to main.js

--- post-grid-and-carousel-ultimate.php
+++ post-grid-and-carousel-ultimate.php
@@ -181,11 +181,7 @@ Final class post_grid_and_carousel_ultimate
         wp_enqueue_style( 'pgcu-swiper-css', PGCU_URL . 'assets/css/swiper-bundle.min.css' );
         wp_enqueue_script( 'pgcu-macy', PGCU_URL . 'assets/js/macy.min.js', array('jquery'), '', true );
         wp_enqueue_script( 'pgcu-swiper', PGCU_URL . 'assets/js/swiper-bundle.min.js', array('jquery') );
-        wp_enqueue_script( 'pgcu-ajax', PGCU_URL . 'assets/js/ajax.js', array('jquery'), '', true );
         wp_enqueue_script( 'pgcu-main-js', PGCU_URL . 'assets/js/main.js', array('jquery') );
-
-
-
     }
 
     /**

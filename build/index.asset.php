ab50af56367da6d38c4afac6d16fe5d0f1b3d8eb
Armanul46 <aazztech4@gmail.com>
2022-11-23 11:15:35 +0600

    fixed pagination issue on gutenberg block

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '474f241b32865e772bb2');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => 'bebc6bf4d2b789c2fdca');

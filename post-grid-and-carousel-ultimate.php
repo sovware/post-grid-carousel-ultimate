a12df9425da1711221d3bc8faaa762e9b71bdbfc
Armanul46 <aazztech4@gmail.com>
2023-01-03 09:18:35 +0600

    updated appsero

--- post-grid-and-carousel-ultimate.php
+++ post-grid-and-carousel-ultimate.php
@@ -3,7 +3,7 @@
 Plugin Name: Post Grid & Carousel Ultimate Pro
 Plugin URI: https://wordpress.org/product/post-grid-carousel-ultimate-pro
 Description: Use Post Grid & Carousel Ultimate Plugin to display your posts in different beautiful Grids and Sliders/Carousels very easily.
-Version: 2.1.0
+Version: 2.1.1
 Author: wpWax
 Author URI: https://wpwax.com
 License: GPLv2 or later
@@ -125,7 +125,7 @@ if( ! in_array('post-grid-carousel-ultimate-pro/post-grid-and-carousel-ultimate.
             if ( ! defined('PGCU_LANG_DIR') ) { define('PGCU_LANG_DIR', dirname(plugin_basename( __FILE__ ) ) . '/languages'); }
             //custom post type id
             if ( ! defined( 'PGC_VERSION' ) ) {
-                define( 'PGC_VERSION', '2.0.0' );
+                define( 'PGC_VERSION', '2.1.1' );
             }
             // this is the URL our updater / license checker pings. This should be the URL of the site with EDD installed
             if ( ! defined( 'PGC_REMOTE_URL' ) ) {
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: wpwax
 Tags: post grid, post carousel, post slider, post sortable grid, grid, carousel
 Requires at least: 4.0
 Tested up to: 6.1
-Stable tag: 2.1.0
+Stable tag: 2.1.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -90,6 +90,8 @@ Post Grid and Carousel Ultimate plugin is very easy to use. Just follow the foll
 15. Backend: Custom Widget
 
 == Changelog ==
+= 2.1.1 =
+* Fix: Security issue
 = 2.1.0 =
 * Add: Elementor Support
 * Add: Gutenberg Support
